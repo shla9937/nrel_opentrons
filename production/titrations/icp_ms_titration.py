21c673e230a07ea8ca14acdccce33e39737ef2fb
Shawn Laursen <shla9937@colorad.edu>
2025-12-10 14:45:31 -0700

    removed extra slots

--- production/titrations/icp_ms_titration.py
+++ production/titrations/icp_ms_titration.py
@@ -24,7 +24,8 @@ metadata = {
     -   Incubate (15 min)
     -   Prep desalt plate
     -   Add acid
-    -   Add desalted protein to acid''',
+    -   Add desalted protein to acid
+    *use Nunc 96 well deep plates, not Nest''',
     'apiLevel': '2.26'}
 
 def run(protocol):
@@ -40,19 +41,17 @@ def run(protocol):
 
 def setup(protocol):
     # equiptment
-    global p300m, tips300, tips300_1, tips300_2, desalt_plate, res1, desalt_elution, rxn_plate, trough, icp_plate, metals, res2
+    global p300m, tips300, tips300_1, desalt_plate, res1, desalt_elution, rxn_plate, trough, icp_plate, metals, res2
     tips300 = protocol.load_labware('opentrons_96_tiprack_300ul', 3)
     tips300_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 9)
-    tips300_2 = protocol.load_labware('opentrons_96_tiprack_300ul', 11)
-    p300m = protocol.load_instrument('p300_multi_gen2', 'left', tip_racks=[tips300, tips300_1, tips300_2])
-    desalt_plate = protocol.load_labware('nest_96_wellplate_2ml_deep', 1) # make custom
+    p300m = protocol.load_instrument('p300_multi_gen2', 'left', tip_racks=[tips300, tips300_1])
+    desalt_plate = protocol.load_labware('nest_96_wellplate_2ml_deep', 1)
     res1 = protocol.load_labware('nest_1_reservoir_195ml', 2)
-    desalt_elution = protocol.load_labware('nest_96_wellplate_2ml_deep', 8) # make custom
     rxn_plate = protocol.load_labware('greiner_96_wellplate_300ul', 5)
     trough = protocol.load_labware('nest_12_reservoir_15ml', 6)
     icp_plate = protocol.load_labware('nest_96_wellplate_2ml_deep', 7)
     metals = protocol.load_labware('opentrons_24_tuberack_nest_1.5ml_screwcap', 4)
-    res2 = protocol.load_labware('nest_1_reservoir_195ml', 10)
+    res2 = protocol.load_labware('nest_1_reservoir_195ml', 8)
 
     global protein, buff, acid
     protein = trough.wells()[0]
@@ -62,11 +61,11 @@ def setup(protocol):
 def pickup_tips(number, pipette, protocol):
     nozzle_dict = {2: "G1", 3: "F1", 4: "E1", 5: "D1", 6: "C1", 7: "B1"}
     if number == 1:
-        p300m.configure_nozzle_layout(style=SINGLE,start="H1", tip_racks=[tips300, tips300_1, tips300_2])
+        p300m.configure_nozzle_layout(style=SINGLE,start="H1", tip_racks=[tips300, tips300_1])
     elif number > 1 and number < 8:
-        p300m.configure_nozzle_layout(style=PARTIAL_COLUMN,start="H1", end=nozzle_dict[number], tip_racks=[tips300, tips300_1, tips300_2])
+        p300m.configure_nozzle_layout(style=PARTIAL_COLUMN,start="H1", end=nozzle_dict[number], tip_racks=[tips300, tips300_1])
     else:
-        p300m.configure_nozzle_layout(style=ALL, tip_racks=[tips300, tips300_1, tips300_2])
+        p300m.configure_nozzle_layout(style=ALL, tip_racks=[tips300, tips300_1])
     p300m.pick_up_tip()
 
 def add_protein(protocol):
