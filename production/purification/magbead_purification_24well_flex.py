6cd7c06b98b2aac41580d100b2219bcf11b15564
Shawn Laursen <shla9937@colorad.edu>
2026-07-14 16:23:49 -0600

    tips in extra slots

--- production/purification/magbead_purification_24well_flex.py
+++ production/purification/magbead_purification_24well_flex.py
@@ -26,17 +26,19 @@ def run(protocol):
 
 def setup(protocol):
     # equipment
-    global trash, pipette, tips1000, tips1000_1, empty_tiprack, tips1000_24well, tips24_adapter, buff, plate, mag_24well
+    global trash, pipette, tips1000, tips1000_1, empty_tiprack, tips1000_24well, tips24_adapter, buff, lysis_plate, mag_24well, bead_plate, liquid_waste
     trash = protocol.load_trash_bin ('A3')
     tips1000 = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'D1')
-    tips1000_1 = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'D2')
+    tips1000_1 = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'D4')
     empty_tiprack = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'B1')
     tips1000_24well = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'A1')
     tips24_adapter = protocol.load_adapter('opentrons_flex_96_tiprack_adapter', 'A2')
     pipette = protocol.load_instrument('flex_96channel_1000')
-    buff = protocol.load_labware('nest_1_reservoir_195ml', 'B3')
+    buff = protocol.load_labware('nest_1_reservoir_195ml', 'C3')
     mag_24well = protocol.load_adapter('shawn_24well_magnet_adapter', 'C2')
-    plate = protocol.load_labware('thomsoninstrument_24_wellplate_10400ul', 'B2')
+    lysis_plate = protocol.load_labware('thomsoninstrument_24_wellplate_10400ul', 'B2')
+    bead_plate = protocol.load_labware('thomsoninstrument_24_wellplate_10400ul', 'C1')
+    liquid_waste = protocol.load_labware('nest_1_reservoir_195ml', 'B3')
 
 def define_liquids(protocol):
     buffer_liquid = protocol.define_liquid(
@@ -44,6 +46,13 @@ def define_liquids(protocol):
         description="Buffer mixture",
         display_color="#50C878")
     buff['A1'].load_liquid(liquid=buffer_liquid,volume=195000)
+
+    lysate_liquid = protocol.define_liquid(
+        name="Lysate",
+        description="Lysed cells",
+        display_color="#FFB347")
+    for well in lysis_plate.wells():
+        well.load_liquid(liquid=lysate_liquid, volume=2000)
 
 def pickup_24(protocol):
     global half_filled
@@ -56,7 +65,13 @@ def pickup_24(protocol):
     if half_filled is False:
         pipette.configure_nozzle_layout(style=protocol_api.ROW,start="H1",tip_racks=[tips1000, tips1000_1])
         for row in range(4):
-            pipette.pick_up_tip()
+            try:
+                pipette.pick_up_tip()
+            except:
+                # active rack ran out; swap the staging-area rack into D1
+                protocol.move_labware(tips1000, "C4", use_gripper=True)
+                protocol.move_labware(tips1000_1, "D1", use_gripper=True)
+                pipette.pick_up_tip()
             pipette.drop_tip(empty_tiprack.rows()[row*2][0])
         half_filled = True
     else:    
@@ -77,12 +92,9 @@ def fill_24well(protocol):
     pipette.drop_tip()
 
 def wash(protocol):
-    fill_24well(protocol)
-    protocol.move_labware(plate, mag_24well, use_gripper=True)
-
-    # remove supernatant 
-    # for sample_well in range(0,24):
-    #     p1000.transfer(1500, well24.wells()[sample_well].bottom(3), deep_well.wells()[sample_well], mix_before=(3,500))
+    pickup_24(protocol)
+    pipette.transfer(2000, lysis_plate.wells()[0].bottom(3), bead_plate.wells()[0], new_tip='never')
+    pipette.drop_tip()
     
     # p1000.move_to(deep_well.wells()[sample_well].top(10))
     # mag_mod.engage(height_from_base=mag_height)
