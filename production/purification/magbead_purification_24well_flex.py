1f73cd8d8b0ced181eb993bed47f3e7c34bb4154
Shawn Laursen <shla9937@colorad.edu>
2026-07-14 14:57:41 -0600

    pickup 24 is now robust

--- production/purification/magbead_purification_24well_flex.py
+++ production/purification/magbead_purification_24well_flex.py
@@ -19,8 +19,8 @@ def run(protocol):
     protocol.set_rail_lights(True)
     setup(protocol)
     define_liquids(protocol)
-    pickup_24(protocol)
-    fill_24well(protocol)
+    for i in range(4):
+        fill_24well(protocol)
     # lyse(protocol)
     # wash(protocol)
     # elute(protocol)
@@ -28,15 +28,16 @@ def run(protocol):
 
 def setup(protocol):
     # equipment
-    global trash, pipette, tips1000, mid_tips1000, tips1000_24well, tips24_adapter, buff, plate
+    global trash, pipette, tips1000, tips1000_1, empty_tiprack, tips1000_24well, tips24_adapter, buff, plate
     trash = protocol.load_trash_bin ('A3')
-    tips1000 = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'B3')
-    mid_tips1000 = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'D1')
-    tips1000_24well = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'D2')
-    tips24_adapter = protocol.load_adapter('opentrons_flex_96_tiprack_adapter', 'C3')
-    pipette = protocol.load_instrument('flex_96channel_1000', tip_racks=[tips1000])
-    buff = protocol.load_labware('nest_1_reservoir_195ml', 'A1')
-    plate = protocol.load_labware('thomsoninstrument_24_wellplate_10400ul', 'B1')
+    tips1000 = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'D1')
+    tips1000_1 = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'D2')
+    empty_tiprack = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'B1')
+    tips1000_24well = protocol.load_labware('opentrons_flex_96_tiprack_1000ul', 'A1')
+    tips24_adapter = protocol.load_adapter('opentrons_flex_96_tiprack_adapter', 'A2')
+    pipette = protocol.load_instrument('flex_96channel_1000')
+    buff = protocol.load_labware('nest_1_reservoir_195ml', 'B2')
+    plate = protocol.load_labware('thomsoninstrument_24_wellplate_10400ul', 'C2')
 
 def define_liquids(protocol):
     buffer_liquid = protocol.define_liquid(
@@ -45,40 +46,33 @@ def define_liquids(protocol):
         display_color="#50C878")
     buff['A1'].load_liquid(liquid=buffer_liquid,volume=195000)
 
-# def pickup_tips(layout, racks, protocol):
-#     if racks is None:
-#         racks = [tips1000, tips1000_2, tips1000_3, tips1000_4]
-#     else:
-#         racks = [racks]
-#     if layout == 'column':
-#         pipette.configure_nozzle_layout(style=protocol_api.COLUMN,start="A12", tip_racks=racks)   
-#     elif layout == 'row':
-#         pipette.configure_nozzle_layout(style=protocol_api.ROW,start="H1",tip_racks=racks)
-#     elif layout == 'single':
-#         pipette.configure_nozzle_layout(style=protocol_api.SINGLE,start="H12",tip_racks=racks)
-#     elif layout == 'all':
-#         pipette.configure_nozzle_layout(style=protocol_api.ALL,start="A1",tip_racks=racks)
-#     else:
-#         raise ValueError("Invalid layout. Choose from 'column', 'row', 'single', or 'all'.")
-#     pipette.pick_up_tip()
-
 def pickup_24(protocol):
-    pipette.configure_nozzle_layout(style=protocol_api.ROW,start="H1",tip_racks=[tips1000])
-    for row in range(4):
-        pipette.pick_up_tip()
-        pipette.drop_tip(mid_tips1000.rows()[row*2][0])
-    
-    mid_tips1000.reset()
-    pipette.configure_nozzle_layout(style=protocol_api.COLUMN,start="A12",tip_racks=[mid_tips1000])
+    global half_filled
+    half_filled = False
+    try:
+        protocol.move_labware(tips1000_24well, "A1", use_gripper=True)
+    except:
+        None
+
+    if half_filled is False:
+        pipette.configure_nozzle_layout(style=protocol_api.ROW,start="H1",tip_racks=[tips1000, tips1000_1])
+        for row in range(4):
+            pipette.pick_up_tip()
+            pipette.drop_tip(empty_tiprack.rows()[row*2][0])
+        half_filled = True
+    else:    
+        half_filled = False
+
+    pipette.configure_nozzle_layout(style=protocol_api.COLUMN,start="A12")
     for col in range(6):
-        pipette.pick_up_tip()
+        pipette.pick_up_tip(empty_tiprack.rows()[0][col])
         pipette.drop_tip(tips1000_24well.rows()[0][col*2])
 
-    tips1000_24well.reset()
     protocol.move_labware(tips1000_24well, tips24_adapter, use_gripper=True)
-    pipette.configure_nozzle_layout(style=protocol_api.ALL,tip_racks=[tips1000_24well])
-    pipette.pick_up_tip()
-
+    pipette.configure_nozzle_layout(style=protocol_api.ALL)
+    pipette.pick_up_tip(tips1000_24well.rows()[0][0])
+    
 def fill_24well(protocol):
+    pickup_24(protocol)
     pipette.transfer(50, buff.wells()[0], plate.wells()[0], new_tip='never')
     pipette.drop_tip()
